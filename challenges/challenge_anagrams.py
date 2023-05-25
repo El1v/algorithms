093836cb3ffed72e433c6071edb199d1ce5167e3
Elivelton <eliveltonmn@outlook.com>
2023-05-25 16:16:22 -0300

    feat: merge_sort order function and is_anagram

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,44 @@
+def merge_sort(numbers, start=0, end=None):
+    if end is None:
+        end = len(numbers)
+    if (end - start) > 1:
+        mid = (start + end) // 2
+        merge_sort(numbers, start, mid)
+        merge_sort(numbers, mid, end)
+        merge(numbers, start, mid, end)
+
+
+def merge(numbers, start, mid, end):
+    left = numbers[start:mid]
+    right = numbers[mid:end]
+
+    left_index, right_index = 0, 0
+
+    for general_index in range(start, end):
+        if left_index >= len(left):
+            numbers[general_index] = right[right_index]
+            right_index = right_index + 1
+        elif right_index >= len(right):
+            numbers[general_index] = left[left_index]
+            left_index = left_index + 1
+        elif left[left_index] < right[right_index]:
+            numbers[general_index] = left[left_index]
+            left_index = left_index + 1
+        else:
+            numbers[general_index] = right[right_index]
+            right_index = right_index + 1
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    first_list = list(first_string.lower())
+    second_list = list(second_string.lower())
+    merge_sort(first_list)
+    merge_sort(second_list)
+    new_first_string = ''.join(first_list)
+    new_second_string = ''.join(second_list)
+
+    if (new_first_string == "" or new_second_string == ""):
+        return (new_first_string, new_second_string, False)
+    if (new_first_string == new_second_string):
+        return (new_first_string, new_second_string, True)
+    return (new_first_string, new_second_string, False)
