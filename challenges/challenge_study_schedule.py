70fe897b646c6c5856a620823419158163f1399d
Elivelton <eliveltonmn@outlook.com>
2023-05-25 14:59:43 -0300

    feat: test_encrypt

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,13 +2,13 @@ def study_schedule(permanence_period, target_time):
     if not target_time:
         return None
     
-    students = 0;
+    students = 0
 
     for start, end in permanence_period:
-        if not all(isinstance(p, int) for p in (start,end)): 
+        if not all(isinstance(p, int) for p in (start, end)): 
             return None
 
-        if(start <= target_time <= end):
+        if (start <= target_time <= end):
             students += 1
 
     return students
