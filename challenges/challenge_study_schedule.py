5582c7e16bf4dfae4c25e1cbc80c6f910b3b1bdf
Elivelton <eliveltonmn@outlook.com>
2023-05-25 14:46:17 -0300

    feat: study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,4 +1,17 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+    if not target_time:
+        return None
+    
+    students = 0;
 
-    raise NotImplementedError
+    for start, end in permanence_period:
+        if not all(isinstance(p, int) for p in (start,end)): 
+            return None
+
+        if(start <= target_time <= end):
+            students += 1
+
+    return students
+
+# link de apoio
+# https://pt.stackoverflow.com/questions/341085/como-validar-se-um-valor-%C3%A9-uma-tupla-possuindo-uma-string-e-um-inteiro
