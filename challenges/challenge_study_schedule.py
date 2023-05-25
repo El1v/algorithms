12711afe5fc0fa16799de6b4388c1cd030946c37
Elivelton <eliveltonmn@outlook.com>
2023-05-25 15:06:46 -0300

    fix: lint

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,11 +1,10 @@
 def study_schedule(permanence_period, target_time):
     if not target_time:
         return None
-    
     students = 0
 
     for start, end in permanence_period:
-        if not all(isinstance(p, int) for p in (start, end)): 
+        if not all(isinstance(p, int) for p in (start, end)):
             return None
 
         if (start <= target_time <= end):
--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -4,11 +4,10 @@ from challenges.challenge_encrypt_message import encrypt_message
 
 def test_encrypt_message():
     assert encrypt_message("teste", 3) == "set_et"
-    assert encrypt_message("teste", 4 ) == "e_tset"
+    assert encrypt_message("teste", 4) == "e_tset"
     assert encrypt_message("teste", 6) == "etset"
 
     with pytest.raises(TypeError, match="tipo inválido para key"):
         encrypt_message("teste", "")
-    
     with pytest.raises(TypeError, match="tipo inválido para message"):
         encrypt_message(10, 1)
